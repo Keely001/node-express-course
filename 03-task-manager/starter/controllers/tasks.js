490d1ba57eb66d1d61b88b836b33dc25b6ab3cc4
Keely001 <thindiuke@gmail.com>
2023-07-02 12:14:56 +0300

    Basic set-up done

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -36,8 +36,21 @@ const getTask = async (req,res) => {
     }
 }
 
-const updateTask = (req,res) => {
-    res.send('updated')
+const updateTask = async (req,res) => {
+    try{
+        const{id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID,}, req.body, {
+            new:true,
+            runValidators:true
+        });
+        if(!task){
+            return res.status(404).json({msg: `no task with id: ${taskID}`})
+        }
+        res.status(200).json({task});
+    }
+    catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 const deleteTask = async (req,res) => {
